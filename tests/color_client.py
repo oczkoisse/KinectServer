692ff932e97b02ee34591e938434c186b49cb0d0
oczkoisse <oczkoisse@pm.me>
2019-03-11 13:35:45 -0600

    Ready to merge jpeg_encoding

--- tests/color_client.py
+++ tests/color_client.py
@@ -4,6 +4,8 @@ import socket, sys, struct
 import time
 import numpy as np
 import matplotlib.pyplot as plt
+import imageio
+import cv2
 
 src_addr = 'localhost'
 src_port = 8000
@@ -20,44 +22,46 @@ def connect():
     try:
         sock.connect((src_addr, src_port))
     except:
-        print("Error connecting to {}:{}".format(src_addr, src_port))
+        print ("Error connecting to {}:{}".format(src_addr, src_port))
         return None
     try:
-        print("Sending stream info")
+        print ("Sending stream info")
         sock.sendall(struct.pack('<iBi', 5, 1, stream_id));
     except:
-        print("Error: Stream rejected")
+        print ("Error: Stream rejected")
         return None
-    print("Successfully connected to host")
+    print ("Successfully connected to host")
     return sock
     
 
 # timestamp | frame type | stride | width | height | color_data
 def decode_frame(raw_frame):
-    
     # Expect little endian byte order
     endianness = "<"
 
     # In each frame, a header is transmitted
     # Timestamp | frame type | width | height
-    header_format = "qiiii"
+    header_format = "qiiiii"
     header_size = struct.calcsize(endianness + header_format)
     header = struct.unpack(endianness + header_format, raw_frame[:header_size])
 
-    timestamp, frame_type, stride, width, height = header
+    timestamp, frame_type, stride, width, height, num_bits = header
+
+    # Removed called to struct.unpack_from and decoded the jpeg file
+    # Relies on the imageio jibrary 
+    print(header)
+    #color_data = imageio.imread(raw_frame[header_size:], "jpeg")
+    color_data = imageio.imread(raw_frame[header_size:])
     
-    color_data_format = str(stride * height) + "B"
-    
-    color_data = struct.unpack_from(endianness + color_data_format, raw_frame, header_size)
-    
-    return (timestamp, frame_type, stride, width, height, list(color_data))
+    return (timestamp, frame_type, stride, width, height, color_data)
 
 def format_as_image(raw_image, width, height):
     # Convert to RGBA from BGRA
     image_as_pixels = np.array([ (raw_image[i+2], raw_image[i+1], raw_image[i], raw_image[i+3]) for i in range(0, len(raw_image), 4)] , dtype=np.float)
+    #image_as_pixels = np.array(raw_image)
     image_as_pixels /= 255.0
     
-    return image_as_pixels.reshape((height, width, 4))
+    return image_as_pixels.reshape((height, width, 3))
 
 def recv_all(sock, size):
     result = b''
@@ -92,28 +96,36 @@ if __name__ == '__main__':
             t_begin = time.time()
             f = recv_color_frame(s)
             t_end = time.time()
-        except:
+        except ex:
+            print(ex)
             s.close()
             break
-        print("Time taken for this frame: {}".format(t_end - t_begin))
+        print ("Time taken for this frame: {}".format(t_end - t_begin))
         avg_frame_time += (t_end - t_begin)
-        timestamp, frame_type, stride, width, height, color_data = decode_frame(f)
-        print(timestamp, frame_type, stride, width, height)
+        timestamp, frame_type, stride, width, height, img = decode_frame(f)
+        #temp = img[:,0]
+        #img[:,0] = img[:,2]
+        #img[:,2] = temp
+        print (timestamp, frame_type, stride, width, height)
 
-        if do_plot and i % 20 == 0:
-            img = format_as_image(color_data, width, height)
-            print(img[0,0])
-            plt.imshow(img)
-            plt.show()
+        if do_plot and i %1  == 0:
+            #img = format_as_image(color_data, width, height)
+            #print (img[0,0])
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            cv2.imshow("David is bad at Overwatch", img)
+            cv2.waitKey(1)
+           # plt.show(block = False)
+           # plt.pause(0.1)
+           # plt.close()
             
-        print("\n\n")
+        print ("\n\n")
         i += 1
 
-    print("Total frame time: {}".format(avg_frame_time))
+    print ("Total frame time: {}".format(avg_frame_time))
 
     avg_frame_time /= i
     
-    print("Average frame time over {} frames: {}".format(i, avg_frame_time))
+    print ("Average frame time over {} frames: {}".format(i, avg_frame_time))
 
     s.close()
     sys.exit(0)
